81653c475a32a5eb19bcdfad20dc80e76e6a0a0b
Lakshay Bhambri <lakshay.bhambri@capiot.com>
2019-06-11 15:47:07 +0530

    Trying to issue on AWS continue 4

--- utils/directoryReader.js
+++ utils/directoryReader.js
@@ -8,18 +8,20 @@ function walkDirectory(dir){
         "value" : dir,
         "children" : []
     }
-    console.log("dir = ",dir);
+    // console.log("dir = ",dir);
     var list = fs.readdirSync(dir);
     if(list.length > 0){
         list.forEach((file) => {
             filePath = path.resolve(dir, file);
             let stat = fs.lstatSync(filePath)
             if (stat.isFile()) {
+                console.log("file = ",filePath)
                 results.children.push({
                     "name" : file,
                     "value" : filePath
                 });
             } else {
+                console.log("directory = ",filePath)
                 results.children.push(walkDirectory(filePath));
             }
         });
